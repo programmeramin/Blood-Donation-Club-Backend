f80ea036cd2929d55051d5e84741ebbdfa767552
programmeramin <developeramin2615@gmail.com>
2025-10-28 21:06:22 +0600

    code updated

--- controller/authController.js
+++ controller/authController.js
@@ -214,8 +214,8 @@ export const login = asyncHandler(async (req, res) => {
 
   res.cookie("loginUserToken", loginUserToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV == "development" ? false : true,
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     path: "/",
     maxAge: 1000 * 60 * 60 * 24 * 365,
   });
